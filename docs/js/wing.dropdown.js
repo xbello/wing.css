ea1e91fe128b03eb4577e4a5bb1a1bee44fbf7f2
Xabier Bello <xbello@gmail.com>
2024-12-04 12:15:34 +0100

    Initial code commit.

---
+++ docs/js/wing.dropdown.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("[data-toggle='dropdown']").forEach(element => {
+ element.addEventListener("click", function() {
+  this.classList.toggle("active");
+  this.nextElementSibling.classList.toggle("show");
+ });
+});
